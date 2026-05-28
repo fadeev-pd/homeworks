9ef1f511f2acb15a3fcc2f64b9db591c2b9076ea
fadeev-pd <fadeev.pd@ya.ru>
2026-05-28 08:19:34 +0000

    .

--- 4.17.cpp
+++ 4.17.cpp
@@ -33,6 +33,10 @@ int main()
     int n, mas[100], x;
     cout << "Введите количество элементов в массиве: " << endl;
     cin >> n;
+    if (n < 3) {
+        cout << "Слишком маленький массив." << endl;
+        return 0;
+    }
     cout << "Введите 1 для ручного заполнения массива и 0 для автоматического: " << endl;
     cin >> x;
     if (x == 1) vvod(n, mas);
@@ -46,7 +50,7 @@ int main()
     cout << endl;
 
     cout << "Номера локальных максимумов: " << endl;
-    for (int i = 1; i < n-1; i++) {
+    for (int i = 1; i < n - 1; i++) {
         if (mas[i] > mas[i - 1] and mas[i] > mas[i + 1]) cout << i + 1 << " ";
     }
 }
